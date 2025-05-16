4f017bddfcfe603b280028889ae45b1b070af572
Lenovo <765536121@qq.com>
2025-05-16 10:06:34 +0800

    联调修改大模型对应接口

--- program/Flask.py
+++ program/Flask.py
@@ -1,18 +1,19 @@
 import json
 import os
+import sys
 import traceback
 
 import pandas as pd
-from flask import Flask, jsonify, request, send_file
-from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtCore import pyqtSignal, QThread
-import sys
-
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from flask import Flask, jsonify, request, send_file, make_response
+from werkzeug.utils  import safe_join
+from flask_cors import CORS
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 task_management_path = os.path.join(BASE_DIR, 'TaskManagement')
 exp_config_path = os.path.join(BASE_DIR, 'config', 'exp_config')
 app = Flask(__name__)
-
+CORS(app, origins=["https://127.0.0.1:8086"])
 
 class FlaskThread(QThread):
     start_experiment_signal = pyqtSignal()
@@ -41,8 +42,9 @@ class FlaskThread(QThread):
         def set_params_api():
             try:
                 exp_id = request.args.get('experiment_id')
-                data = request.json
-                print(f'Received data:', data)
+                dataStr = request.json
+                print(f'Received data:', dataStr)
+                data = json.loads(dataStr)
                 df = pd.DataFrame(data)
                 filepath = os.path.join(exp_config_path, f'{exp_id}.xlsx')
                 df.to_excel(filepath, index=False)
@@ -66,12 +68,26 @@ class FlaskThread(QThread):
             flag = 200
             message = "实验执行完成"
             for entity in data:
-                if entity["progress"] == "停止中" or entity["progress"] == "进行中":
+                if entity["task_status"] == "未开始":
+                    entity["status_code"] = "01"
+                if entity["task_status"] == "进行中":
                     flag = 201
                     message = "实验进行中"
-                if entity["progress"] == "实验执行异常":
+                    entity["status_code"] = "02"
+                if entity["task_status"] == "停止中":
+                    flag = 201
+                    message = "实验进行中"
+                    entity["status_code"] = "03"
+                if entity["task_status"] == "已完成":
+                    entity["status_code"] = "04"
+                if entity["task_status"] == "实验执行异常":
                     flag = 500
                     message = "任务id" + str(entity["task_id"]) + "执行失败"
+                    entity["status_code"] = "05"
+                if entity["start_time"] is None:
+                    entity["progress"] = "0%"
+                    entity["task_status"] = "未开始"
+                    entity["status_code"] = "01"
             # 返回 JSON 数据
             return jsonify({"result": {"data": data, "message": "状态已返回"}, "code": flag, "message": message}), flag
 
@@ -83,13 +99,17 @@ class FlaskThread(QThread):
                 filepath = os.path.join(task_management_path, "实验任务进度管理表.csv")
                 df = pd.read_csv(filepath, na_filter=False)
                 print(f'df: {df}')
-                filtered_df = df[(df['experiment_id'] == exp_id) & (df['task_id'] == task_id)]
+                filtered_df = df[(df['experiment_id'] == int(exp_id)) & (df['task_id'] == task_id)]
                 print(f'filtered_df: {filtered_df}')
                 if not filtered_df.empty:
                     filepath = filtered_df['image_result'].values[0]
                     print(f"图像结果: {filepath}")
                     if filepath:
-                        return send_file(filepath, as_attachment=True)
+                        response = make_response(send_file(filepath, mimetype="image/jpeg", as_attachment=True, conditional=False))
+                        # 添加跨域和缓存头
+                        response.headers['Access-Control-Allow-Origin'] = '*'
+                        response.headers['Cache-Control'] = 'no-store'  # 禁止缓存
+                        return response
                 else:
                     print("没有找到匹配的行。")
             except Exception as e:
@@ -104,13 +124,17 @@ class FlaskThread(QThread):
                 filepath = os.path.join(task_management_path, "实验任务进度管理表.csv")
                 df = pd.read_csv(filepath, na_filter=False)
                 print(f'df: {df}')
-                filtered_df = df[(df['experiment_id'] == exp_id) & (df['task_id'] == task_id)]
+                filtered_df = df[(df['experiment_id'] == int(exp_id)) & (df['task_id'] == task_id)]
                 print(f'filtered_df: {filtered_df}')
                 if not filtered_df.empty:
                     filepath = filtered_df['video_result'].values[0]
                     print(f"视频结果: {filepath}")
                     if filepath:
-                        return send_file(filepath, as_attachment=True)
+                        response = make_response(send_file(filepath, mimetype="video/mp4", as_attachment=True, conditional=False))
+                        # 添加跨域和缓存头
+                        response.headers['Access-Control-Allow-Origin'] = '*'
+                        response.headers['Cache-Control'] = 'no-store'  # 禁止缓存
+                        return response
                 else:
                     print("没有找到匹配的行。")
             except Exception as e:
