f59e822172a70c0d6e95bc8b8d122571697e3f7b
Lenovo <765536121@qq.com>
2025-06-12 11:32:42 +0800

    自动对焦功能

--- program/aicvd.py
+++ program/aicvd.py
@@ -3,6 +3,7 @@ import csv
 import json
 import os
 import sys
+import threading
 import time
 import traceback
 from datetime import datetime, time as dt_time
@@ -15,7 +16,8 @@ import numpy as np
 import pandas as pd
 import qdarktheme
 import serial
-from PyQt5.QtCore import QTimer, QSignalBlocker, Qt, pyqtSignal, QUrl, QCoreApplication, QPoint, QLineF, QSize, QTime
+from PyQt5.QtCore import QTimer, QSignalBlocker, Qt, pyqtSignal, QUrl, QCoreApplication, QPoint, QLineF, QSize, QTime, \
+    QObject
 from PyQt5.QtGui import QImage, QPixmap, QPainter, QPen, QFont, QColor
 from PyQt5.QtWidgets import (QMainWindow, QMessageBox, QMenu, QAction, QApplication, QDialog, QFileDialog,
                              QTreeWidgetItem, QLabel, QToolBar, QPushButton, QLineEdit, QButtonGroup, QRadioButton,
@@ -48,6 +50,12 @@ task_management_path = os.path.join(BASE_DIR, 'TaskManagement')
 
 class AICVD(QMainWindow, Ui_MainWindow):
     evtCallback = pyqtSignal(int)
+
+    # 添加信号
+    snap_signal = pyqtSignal()  # 手动拍照信号
+    snap_auto_signal = pyqtSignal()  # 自动对焦拍照信号
+    focus_up_signal = pyqtSignal()
+    focus_down_signal = pyqtSignal()
 
     def __init__(self, parent=None):
         super(AICVD, self).__init__(parent)
@@ -121,6 +129,25 @@ class AICVD(QMainWindow, Ui_MainWindow):
         self.timer_main = QTimer(self)
         self.timer_main.timeout.connect(self.main_loop)
         self.timer_main.start(1000)
+
+        # 自动对焦相关
+        self.autoFocusFlag = False
+        self.auto_focus_thread = None
+        self.target_dir = None
+
+        # 初始化自动对焦工作器
+        self.auto_focus_worker = AutoFocusWorker(self)
+
+        # 连接信号
+        self.snap_signal.connect(self.onBtnSnap)  # 手动拍照
+        self.snap_auto_signal.connect(self.onBtnSnapAuto)  # 自动对焦拍照
+        self.focus_up_signal.connect(self.onFocusUp)
+        self.focus_down_signal.connect(self.onFocusDown)
+
+        # 连接自动对焦信号
+        self.auto_focus_worker.progress.connect(self.on_auto_focus_progress)
+        self.auto_focus_worker.sharpness_calculated.connect(self.on_sharpness_calculated)
+        self.auto_focus_worker.finished.connect(self.on_auto_focus_finished)
 
     def onConnectFocus(self):
         if self.focus_worker:
@@ -1702,15 +1729,72 @@ class AICVD(QMainWindow, Ui_MainWindow):
                         qimage.save(f'{out_image_path2}')
                         print(f'save image to {out_image_path2}')
                 else:
+                    # 手动操作时显示分辨率选择菜单
                     menu = QMenu()
                     for i in range(0, self.cur.model.still):
-                        action = QAction("{}*{}".format(self.cur.model.res[i].width, self.cur.model.res[i].height), self)
+                        action = QAction("{}*{}".format(self.cur.model.res[i].width, self.cur.model.res[i].height),
+                                         self)
                         action.setData(i)
                         menu.addAction(action)
                     action = menu.exec(self.btn_snap.mapToGlobal(QPoint(0, self.btn_snap.height())))
-                    self.hcam.Snap(action.data())
+                    if action:  # 确保用户选择了分辨率
+                        self.hcam.Snap(action.data())
             except Exception as e:
                 print(f'{e}')
+
+    def onBtnSnapAuto(self, target_width=2592, target_height=1944):
+        """自动对焦时使用的拍照方法，默认使用指定分辨率"""
+        if self.hcam:
+            try:
+                if 0 == self.cur.model.still:  # not support still image capture
+                    if self.pData is not None:
+                        image = QImage(self.pData, self.imgWidth, self.imgHeight, QImage.Format_RGB888)
+                        self.count += 1
+                        dtime = datetime.fromtimestamp(time.time()).strftime('%H%M%S')
+                        out_image_path = os.path.join(image_directory_path,
+                                                      f'{self.exp_id}_{self.order}_{dtime}_{self.count}.jpg')
+                        image.save(f'{out_image_path}')
+                        print(f'save image to {out_image_path}')
+                        mag = int(self.cmb_magnification.currentText())
+                        val = int(self.slider_mag.value())
+                        self.line_len = mag / 20 * 421 * val / 100
+                        text = f'{val}μm'
+                        qimage = self.add_text_to_image(image, text,
+                                                        QPoint(image.width() - int(self.line_len) - 50,
+                                                               image.height() - 30),
+                                                        20)
+                        qimage = self.draw_line_with_arrows(qimage, QPoint(image.width() - int(self.line_len) - 50,
+                                                                           image.height() - 10),
+                                                            QPoint(image.width() - 50, image.height() - 10))
+                        out_image_path2 = os.path.join(image_directory_path,
+                                                       f'{self.exp_id}_{self.order}_{dtime}_{self.count}_scale.jpg')
+                        qimage.save(f'{out_image_path2}')
+                        print(f'save image to {out_image_path2}')
+                else:
+                    # 自动对焦时使用默认分辨率，不显示菜单
+                    default_resolution_index = -1
+
+                    # 查找匹配的分辨率索引
+                    for i in range(0, self.cur.model.still):
+                        if (self.cur.model.res[i].width == target_width and
+                                self.cur.model.res[i].height == target_height):
+                            default_resolution_index = i
+                            break
+
+                    # 如果找到了目标分辨率，使用它；否则使用第一个可用分辨率
+                    if default_resolution_index != -1:
+                        print(f'自动对焦使用分辨率: {target_width}*{target_height}')
+                        self.hcam.Snap(default_resolution_index)
+                    else:
+                        # 如果没有找到目标分辨率，打印可用分辨率并使用第一个
+                        print(f'未找到分辨率 {target_width}*{target_height}，可用分辨率:')
+                        for i in range(0, self.cur.model.still):
+                            print(f'  {i}: {self.cur.model.res[i].width}*{self.cur.model.res[i].height}')
+                        print(
+                            f'自动对焦使用第一个可用分辨率: {self.cur.model.res[0].width}*{self.cur.model.res[0].height}')
+                        self.hcam.Snap(0)
+            except Exception as e:
+                print(f'自动拍照时出错: {e}')
 
     def onBtnSnap2(self):
         try:
@@ -1830,13 +1914,18 @@ class AICVD(QMainWindow, Ui_MainWindow):
                 dtime = datetime.fromtimestamp(time.time()).strftime('%H%M%S')
                 self.out_video_path = os.path.join(video_directory_path, f'{self.exp_id}_{self.order}_{dtime}.mp4')
                 self.out_video_path2 = os.path.join(video_directory_path, f'{self.exp_id}_{self.order}_{dtime}_mix.mp4')
-                # fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-                fourcc = cv2.VideoWriter_fourcc(*'X264')  # 改为H264格式
+
+                # x264供平台和大模型侧使用，日常使用mp4v格式
+                fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+                # fourcc = cv2.VideoWriter_fourcc(*'X264')  # 改为H264格式
                 nFrame, nTime, nTotalFrame = self.hcam.get_FrameRate()
                 frame_rate = nFrame * 1000.0 / nTime
                 resolution = (self.imgWidth, self.imgHeight)
                 self.video_writer = cv2.VideoWriter(self.out_video_path, fourcc, frame_rate, resolution)
                 self.video_writer2 = cv2.VideoWriter(self.out_video_path2, fourcc, frame_rate, resolution)
+                # 开启自动对焦
+                self.startAutoFocus()
+                self.autoFocusFlag = True
 
     def stop_recording(self):
         if self.hcam:
@@ -1861,6 +1950,9 @@ class AICVD(QMainWindow, Ui_MainWindow):
                 self.cacheTempData = np.empty((0, 5))
                 self.cacheMFCData = np.empty((0, 3))
                 self.combinedData = None
+                # 关闭自动对焦
+                self.stopAutoFocus()
+                self.autoFocusFlag = False
 
     def exportExcel(self):
         try:
@@ -2025,6 +2117,7 @@ class AICVD(QMainWindow, Ui_MainWindow):
                     #新建当日文件夹
                     dtime = datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d")
                     today_folder = datetime.now().strftime("%Y-%m-%d")  # 创建当日日期目录
+
                     target_dir = os.path.join(image_directory_path, today_folder)  # 构建完整存储路径
                     os.makedirs(target_dir, exist_ok=True)  # 自动创建目录
                     image.save(os.path.join(target_dir, f'{self.exp_id}_{self.order}_{dtime}_{self.count}.jpg'))
@@ -2038,9 +2131,17 @@ class AICVD(QMainWindow, Ui_MainWindow):
                     qimage = self.draw_line_with_arrows(qimage, QPoint(image.width() - int(self.line_len) - 50,
                                                                        image.height() - 10),
                                                         QPoint(image.width() - 50, image.height() - 10))
-                    out_image_path2 = os.path.join(image_directory_path, f'{self.exp_id}_{self.order}_{dtime}_{self.count}_mix.jpg')
-                    qimage.save(f'{out_image_path2}')
-                    print(f'save image to {out_image_path2}')
+
+                    # out_image_path2 = os.path.join(image_directory_path, f'{self.exp_id}_{self.order}_{dtime}_{self.count}_mix.jpg')
+                    # qimage.save(f'{out_image_path2}')
+                    # print(f'save image to {out_image_path2}')
+
+                    image.save(os.path.join(target_dir, f'{self.exp_id}_{self.order}_{dtime}_{self.count}_mix.jpg'))
+                    print(f'save image to {target_dir}')
+
+                    self.target_dir = target_dir
+                    image.save(os.path.join(target_dir, 'auto_focus.jpg'))
+
 
     def add_text_to_image(self, image, text, position, size):
         # 创建一个新的QImage对象，内容是原始图像的副本
@@ -2291,6 +2392,29 @@ class AICVD(QMainWindow, Ui_MainWindow):
             self.focus_worker = None
         super().closeEvent(event)
 
+    def startAutoFocus(self):
+        """开始自动对焦"""
+        self.auto_focus_worker.start_auto_focus()
+
+    def stopAutoFocus(self):
+        """停止自动对焦"""
+        self.auto_focus_worker.stop_auto_focus()
+
+    def on_auto_focus_progress(self, message):
+        """处理自动对焦进度信息"""
+        print(f"自动对焦: {message}")
+        # 可以在这里更新UI显示进度
+
+    def on_sharpness_calculated(self, sharpness):
+        """处理清晰度计算结果"""
+        print(f"当前清晰度: {sharpness:.2f}")
+        # 可以在这里更新UI显示清晰度
+
+    def on_auto_focus_finished(self):
+        """自动对焦完成"""
+        print("自动对焦完成")
+        # 可以在这里更新UI状态
+
     def main_loop(self):
         # 实验前先清洗MFC设备{self.counter}秒
         if self.IsLaunched:
@@ -2335,7 +2459,6 @@ class AICVD(QMainWindow, Ui_MainWindow):
             self.send_default_signal()
 
         if self.IsExpEnd and not self.IsResultSaved:
-            print("1111")
             # 保存结果
             self.out_image_path = self.onBtnSnap2()
             self.onSaveResult()
@@ -2357,6 +2480,171 @@ class AICVD(QMainWindow, Ui_MainWindow):
                 self.restartFlag = False
         if self.IsExpEnd and self.restartFlag:
             self.start_experiment()
+
+
+class AutoFocusWorker(QObject):
+    """自动对焦工作类，使用信号槽机制避免线程阻塞"""
+    finished = pyqtSignal()
+    progress = pyqtSignal(str)
+    sharpness_calculated = pyqtSignal(float)
+
+    def __init__(self, parent):
+        super().__init__()
+        self.parent = parent
+        self.running = False
+        self.current_step = 0
+        self.max_steps = 15  # 减少最大步数
+        self.best_sharpness = 0
+        self.best_position = 0
+        self.current_direction = -1
+        self.improvement_threshold = 0.01  # 降低阈值
+        self.no_improvement_count = 0
+        self.max_no_improvement = 20
+
+        # 使用定时器控制执行节奏
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.process_step)
+        self.timer.setSingleShot(True)
+
+    def calculate_sharpness(self, image_path):
+        """计算图片清晰度，增加错误处理"""
+        try:
+            if not os.path.exists(image_path):
+                self.progress.emit(f"图片文件不存在: {image_path}")
+                return 0
+
+            # 等待文件完全写入
+            time.sleep(0.2)
+
+            image = cv2.imread(image_path)
+            if image is None:
+                self.progress.emit(f"无法读取图片: {image_path}")
+                return 0
+
+            # 转换为灰度图
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+            # 应用拉普拉斯算子
+            laplacian = cv2.Laplacian(gray, cv2.CV_64F)
+
+            # 计算方差作为清晰度分数
+            sharpness = laplacian.var()
+
+            return sharpness
+
+        except Exception as e:
+            self.progress.emit(f"计算清晰度时出错: {e}")
+            return 0
+
+    def start_auto_focus(self):
+        """开始自动对焦"""
+        if self.running:
+            self.progress.emit("自动对焦已在运行中")
+            return
+
+        self.running = True
+        self.current_step = 0
+        self.best_sharpness = 0
+        self.best_position = 0
+        self.current_direction = 0
+        self.no_improvement_count = 0
+
+        self.progress.emit("开始自动对焦...")
+        self.timer.start(100)  # 100ms后开始第一步
+
+    def stop_auto_focus(self):
+        """停止自动对焦"""
+        self.running = False
+        self.timer.stop()
+        self.progress.emit("自动对焦已停止")
+        self.finished.emit()
+
+    def process_step(self):
+        """处理单步对焦"""
+        if not self.running:
+            self.stop_auto_focus()
+            return
+
+        try:
+            # 步骤1：拍摄快照（使用自动对焦专用方法）
+            self.progress.emit(f"步骤 {self.current_step + 1}: 拍摄快照...")
+
+            # 使用自动对焦专用的拍照信号
+            self.parent.snap_auto_signal.emit()
+
+            # 等待快照完成后继续
+            QTimer.singleShot(1000, self.analyze_snapshot)
+
+        except Exception as e:
+            self.progress.emit(f"处理步骤时出错: {e}")
+            self.stop_auto_focus()
+
+    def analyze_snapshot(self):
+        """分析快照清晰度"""
+        try:
+            image_dir = os.path.join(self.parent.target_dir, "auto_focus.jpg")
+
+            # 计算清晰度
+            sharpness = self.calculate_sharpness(image_dir)
+            self.sharpness_calculated.emit(sharpness)
+            self.progress.emit(f"清晰度: {sharpness:.2f}")
+
+            # 分析清晰度变化
+            self.analyze_sharpness_change(sharpness)
+
+        except Exception as e:
+            self.progress.emit(f"分析快照时出错: {e}")
+            self.stop_auto_focus()
+
+    def analyze_sharpness_change(self, sharpness):
+        """分析清晰度变化并决定下一步动作"""
+        try:
+            if self.current_step == 0:
+                self.best_sharpness = sharpness
+                self.best_position = self.current_step
+            elif sharpness > self.best_sharpness + self.improvement_threshold:
+                self.best_sharpness = sharpness
+                self.best_position = self.current_step
+                self.no_improvement_count = 0
+                self.progress.emit("清晰度改善")
+            elif sharpness < self.best_sharpness - self.improvement_threshold:
+                self.current_direction *= -1
+                self.no_improvement_count += 1
+                self.progress.emit("清晰度下降，改变方向")
+            else:
+                self.no_improvement_count += 1
+
+            # 检查是否应该停止
+            if not self.parent.autoFocusFlag:
+                self.progress.emit(f"连续{self.max_no_improvement}步无改善，停止对焦")
+                self.stop_auto_focus()
+                return
+
+            # 执行对焦调整
+            self.adjust_focus()
+
+        except Exception as e:
+            self.progress.emit(f"分析清晰度变化时出错: {e}")
+            self.stop_auto_focus()
+
+    def adjust_focus(self):
+        """调整对焦"""
+        try:
+            if self.current_direction > 0:
+                self.parent.focus_up_signal.emit()
+                self.progress.emit("向上调整对焦")
+            else:
+                self.parent.focus_down_signal.emit()
+                self.progress.emit("向下调整对焦")
+
+            self.current_step += 1
+
+            # 等待对焦调整完成后继续下一步
+            QTimer.singleShot(1500, self.process_step)  # 增加等待时间
+
+        except Exception as e:
+            self.progress.emit(f"调整对焦时出错: {e}")
+            self.stop_auto_focus()
 
 
 if __name__ == '__main__':
